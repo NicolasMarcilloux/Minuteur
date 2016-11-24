6fb695a9eab82a1bc91d6af3794a7c0d1ba023bb
Marcilloux Nicolas <nmar33@free.fr>
2016-11-24 01:13:06 +0100

    test

--- Tarot/src/App/App.java
+++ Tarot/src/App/App.java
@@ -1,9 +1,28 @@
 package App;
+import Controller.Controller;
+import Model.*;
+import View.View;
 
 public class App {
 
 	public static void main(String[] args) {
-		System.out.print("Hello world");
+		Color couleur = null;
+		Model model = new Model();
+		View view = new View();
+		Controller c = new Controller();
+		
+		
+		Player p = new Player("Yoann");
+		NormalCard nc = new NormalCard(10, Color.Coeur);
+		NormalCard nc2 = new NormalCard('R', Color.Carreau);
+		TrumpCard tc = new TrumpCard(20);
+		
+		
+		p.getCard(tc);
+		p.getCard(nc);
+		p.getCard(nc2);
+		
+		p.showHand();
 	}
 
 }
